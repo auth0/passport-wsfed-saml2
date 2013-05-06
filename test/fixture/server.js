f9a872a58432c244561d18a1d35d948aeaf3d1e0
Matias Woloski <matiasw@gmail.com>
2013-05-06 18:05:51 -0300

    fix issue when request is post but there is no wresult, added integration tests

---
+++ test/fixture/server.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var http = require('http');
+var Strategy = require('../../lib/passport-wsfed-saml2').Strategy;
+var passport = require('passport');
+var wsfed = require('wsfed');
+var xtend = require('xtend');
+var fs = require('fs');
+var path = require('path');
+
+passport.use(new Strategy(
+  {
+    path: '/callback',
+    realm: 'urn:fixture-test',
+    identityProviderUrl: 'http://localhost:5050/login',
+    thumbprint: '5ca6e1202eafc0a63a5b93a43572eb2376fed309'
+  },
+  function(profile, done) {
+    return done(null, profile);
+  })
+);
+
+var fakeUser = {
+  id: '12345678',
+  displayName: 'John Foo',
+  name: {
+    familyName: 'Foo',
+    givenName: 'John'
+  },
+  emails: [
+    {
+      type: 'work',
+      value: 'jfoo@gmail.com'
+    }
+  ]
+};
+
+var credentials = {
+  cert:     fs.readFileSync(path.join(__dirname, '../test-auth0.pem')),
+  key:      fs.readFileSync(path.join(__dirname, '../test-auth0.key'))
+};
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+module.exports.options = {};
+
+module.exports.start = function(options, callback){
+  module.exports.options = options;
+  if (typeof options === 'function') {
+    callback = options;
+    module.exports.options = {};
+  }
+
+  var app = express();
+
+  app.configure(function(){
+    this.use(express.bodyParser());
+    this.use(passport.initialize());
+    this.use(passport.session());
+    this.use(function(req,res,next){
+      req.user = fakeUser;
+      next();
+    });
+  });
+
+  function getPostURL (wtrealm, wreply, req, callback) {
+    callback(null, 'http://localhost:5050/callback');
+  }
+
+  app.get('/login', 
+    wsfed.auth(xtend({}, {
+      issuer:             'fixture-test',
+      getPostURL:         getPostURL,
+      cert:               credentials.cert,
+      key:                credentials.key
+  }, options)));
+
+  app.post('/callback',
+    passport.authenticate('wsfed-saml2'),
+    function(req, res) {
+      res.json(req.user);
+    });
+
+
+  var server = http.createServer(app).listen(5050, callback);
+  module.exports.close = server.close.bind(server);
+};
+
+module.exports.fakeUser = fakeUser;
+module.exports.credentials = credentials;
---
+++ test/wsfed.tests.js
@@ -0,0 +1,101 @@
+var expect = require('chai').expect;
+var server = require('./fixture/server');
+var request = require('request');
+var cheerio = require('cheerio');
+
+describe('wsfed', function () {
+  before(function (done) {
+    server.start(done);
+  });
+  
+  after(function (done) {
+    server.close(done);
+  });
+
+  describe('normal flow', function () {
+    var user, r, bod, $;
+
+    before(function (done) {
+      request.get({
+        jar: request.jar(), 
+        uri: 'http://localhost:5050/login?wa=wsignin1.0&wtrealm=urn:fixture-test'
+      }, function (err, response, b){
+        if(err) return done(err);
+        expect(response.statusCode)
+          .to.equal(200);
+
+
+        $ = cheerio.load(b);
+        var wresult = $('input[name="wresult"]').attr('value');
+        var wa = $('input[name="wa"]').attr('value');
+        request.post({
+          jar: request.jar(), 
+          uri: 'http://localhost:5050/callback',
+          form: { wresult: wresult, wa: wa }
+        }, function(err, response, body) {
+          if(err) return done(err);
+          r = response;
+          bod = body;
+          done();
+        });
+      });
+    });
+
+    it('should be valid signature', function(){
+      expect(r.statusCode)
+            .to.equal(200);
+    });
+
+    it('should return a valid user', function(){
+      var user = JSON.parse(bod);
+      expect(user['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'])
+          .to.equal(server.fakeUser.id);
+      expect(user['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'])
+          .to.equal(server.fakeUser.emails[0].value);
+    });
+
+  });
+
+  describe('missing wresult in POST', function () {
+    var user, r, bod, $;
+
+    before(function (done) {
+      request.post({
+        jar: request.jar(), 
+        uri: 'http://localhost:5050/callback'
+      }, function(err, response, body) {
+        if(err) return done(err);
+        r = response;
+        bod = body;
+        done();
+      });
+    });
+
+    it('should return a 400', function(){
+      expect(r.statusCode)
+            .to.equal(400);
+    });
+  });
+
+  describe('invalid wresult in POST', function () {
+    var user, r, bod, $;
+
+    before(function (done) {
+      request.post({
+        jar: request.jar(), 
+        uri: 'http://localhost:5050/callback',
+        form: { wresult: 'foo' }
+      }, function(err, response, body) {
+        if(err) return done(err);
+        r = response;
+        bod = body;
+        done();
+      });
+    });
+
+    it('should return a 400', function(){
+      expect(r.statusCode)
+            .to.equal(400);
+    });
+  });
+});
