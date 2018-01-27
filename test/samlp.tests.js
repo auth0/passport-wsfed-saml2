35d0a4005476dc06022458621fab50a9e9da8bff
Mike Lee <mike.lee@auth0.com>
2018-01-26 20:16:01 -0800

    Added tests for the encoded XML value in an AttributeValue field

--- test/samlp.tests.js
+++ test/samlp.tests.js
@@ -450,6 +450,27 @@ describe('samlp (unit tests)', function () {
       });
     });
 
+    it('should accept the signature when the saml response has an embedded XML assertion', function(done){
+      var encodedSamlResponse = fs.readFileSync(__dirname + '/samples/encoded/samlresponse_encoded_xml.txt').toString();
+      var cert = fs.readFileSync(__dirname + '/test-auth0-2.cer').toString();
+      const samlResponse = new Buffer(encodedSamlResponse, 'base64').toString();
+      var options = {
+        cert: cert,
+        checkExpiration: false,
+        checkDestination: false,
+        checkRecipient: false,
+        checkAudience: false,
+        checkSPNameQualifier: false
+      };
+      var samlp = new Samlp(options, new Saml(options));
+
+      samlp.validateSamlResponse(samlResponse, function (err, profile) {
+        if (err) return done(err);
+        expect(profile).to.be.ok;
+        done();
+      });
+    });
+
     it('should digest has an extra space', function(done){
       var encodedSamlResponse = fs.readFileSync(__dirname + '/samples/encoded/samlresponse_extraspace.txt').toString();
       var cert = fs.readFileSync(__dirname + '/test-auth0.cer').toString();
